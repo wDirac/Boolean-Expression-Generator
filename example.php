fbf4bfda6aa268da2b0114001274b4ded68327b7
Paul <Prueba12345c@gmail.com>
2018-04-10 21:24:35 +0200

    Update example.php

--- example.php
+++ example.php
@@ -3,13 +3,15 @@
 require_once('bgen_lib.php');
 ini_set('memory_limit', -1);
 
+$file_name = 'test.txt';
+
 $mode = [6540,'RC','N-R'];
 $symbols = ['A','B', 'C', 'D', 'E', 'F', 'Q', 'j', 'O', 'Z'];
 $operators_form = ['(',')', ' x ', ' + ', "'"];
 
 $data = gen_block($mode, $symbols, $operators_form);
 
-write('test.txt', $data);
+write($file_name, $data);
 
 
 ?>
