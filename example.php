d7f4687b718fecc46de0edef91ad4ada311101d4
wDirac <Prueba12345c@gmail.com>
2018-04-10 15:25:00 +0200

    Update and rename lib.php to example.php

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('bgen_lib.php);
+ini_set('memory_limit', -1);
+
+$mode = [6540,'RC','N-R'];
+$symbols = ['A','B', 'C', 'D', 'E', 'F', 'Q', 'j', 'O', 'Z'];
+$operators_form = ['(',')', ' x ', ' + ', "'"];
+
+$data = gen_block($mode, $symbols, $operators_form);
+
+write('test.bn', $data);
+
+
+?>
